928c438b0fd310e6f32d5c9d62a93b484ade339b
有種 <39034242+kenzok8@users.noreply.github.com>
2022-12-08 21:02:49 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -109,6 +109,7 @@ git clone --depth 1 https://github.com/lisaac/luci-app-dockerman dockerman && mv
 git clone --depth 1 https://github.com/doushang/luci-app-shortcutmenu luci-shortcutmenu && mv -n luci-shortcutmenu/luci-app-shortcutmenu ./ ; rm -rf luci-shortcutmenu
 git clone --depth 1 https://github.com/Huangjoe123/luci-app-eqos
 
+svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-fileassistant
 svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-filebrowser
 svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-aliddns
